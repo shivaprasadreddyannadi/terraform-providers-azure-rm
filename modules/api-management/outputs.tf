a22eb6623820cef518dc401ff3c1ee31e2cf8ecf
Shiva Annadi <shivaprasad.annadi@gmail.com>
2026-01-30 16:18:19 -0600

    added terraform modules

---
+++ modules/api-management/outputs.tf
@@ -0,0 +1,40 @@
+output "service_id" {
+  description = "The ID of the API Management Service you created"
+  value       = azurerm_api_management.apim_service.id
+}
+
+output "gateway_url" {
+  description = "The URL of the Gateway for the API Management you Service"
+  value       = azurerm_api_management.apim_service.gateway_url
+}
+
+output "service_public_ip_addresses" {
+  description = "The Public IP addresses of the API Management you Service"
+  value       = azurerm_api_management.apim_service.public_ip_addresses
+}
+
+output "api_outputs" {
+  description = "The IDs, state, and version outputs of the APIs you created"
+  value = {
+    id             = azurerm_api_management_api.api.id
+    is_current     = azurerm_api_management_api.api.is_current
+    is_online      = azurerm_api_management_api.api.is_online
+    version        = azurerm_api_management_api.api.version
+    version_set_id = azurerm_api_management_api.api.version_set_id
+  }
+}
+
+output "product_ids" {
+  description = "The ID of the Product you created"
+  value       = azurerm_api_management_product.product.id
+}
+
+output "product_api_ids" {
+  description = "The ID of the Product/API association you created"
+  value       = azurerm_api_management_product_api.product_api.id
+}
+
+output "product_group_ids" {
+  description = "The ID of the Product/Group association you created"
+  value       = azurerm_api_management_product_group.product_group.id
+}
---
+++ modules/container-registry/variables.tf
@@ -0,0 +1,7 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this examples"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this examples should be created"
+}
